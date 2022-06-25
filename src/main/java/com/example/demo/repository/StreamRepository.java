4aa47ef754a81b85116e3433236ae850289b095c
sphokuhle <sphogit@gmail.com>
2022-06-25 20:42:48 +0200

    Äll things Docker and kubernetes

--- src/main/java/com/example/demo/repository/StreamRepository.java
+++ src/main/java/com/example/demo/repository/StreamRepository.java
@@ -17,10 +17,10 @@ public interface StreamRepository  extends PagingAndSortingRepository<Stream, Lo
     @Query(value = "Select s.name, s.code FROM Stream s")
     List<Object[]> getNamesAndCodes();
 
-    @Query(value = "Select Count(DISTINCT(s.stream_id)) AS STREAMS, SUM(case when s.stream_name ='Maths & Science' then 1 else 0 end) AS SCIENCE, SUM(case when s.stream_name ='ALL' then 1 else 0 end) AS ALL_STREAMS, SUM(case when sbjct.subject_code = 'ACC' or sbjct.subject_code ='BM' or sbjct.subject_code ='ECO'  then 1 else 0 end) AS COMMERCE_SUBJECTS FROM APP.STREAM s LEFT JOIN APP.SUBJECT sbjct ON s.stream_id = sbjct.stream_id", nativeQuery = true)
+    @Query(value = "Select Count(DISTINCT(s.stream_id)) AS STREAMS, SUM(case when s.stream_name ='MATHS & SCIENCE' then 1 else 0 end) AS SCIENCE, SUM(case when s.stream_name ='ALL' then 1 else 0 end) AS ALL_STREAMS, SUM(case when sbjct.subject_code = 'ACC' or sbjct.subject_code ='BUSMG' or sbjct.subject_code ='ECO'  then 1 else 0 end) AS COMMERCE_SUBJECTS FROM APP.STREAM s LEFT JOIN APP.SUBJECT sbjct ON s.stream_id = sbjct.stream_id", nativeQuery = true)
     List<Object[]> getResults();
 
-    @Query(value = "Select Count(DISTINCT(s.stream_id)) AS STREAMS, SUM(case when s.stream_name ='Maths & Science' then 1 else 0 end) AS SCIENCE, SUM(case when s.stream_name ='ALL' then 1 else 0 end) AS ALL_STREAMS, SUM(case when sbjct.subject_code = 'ACC' or sbjct.subject_code ='BM' or sbjct.subject_code ='ECO'  then 1 else 0 end) AS COMMERCE_SUBJECTS FROM APP.STREAM s LEFT JOIN APP.SUBJECT sbjct ON s.stream_id = sbjct.stream_id", nativeQuery = true)
+    @Query(value = "Select Count(DISTINCT(s.stream_id)) AS STREAMS, SUM(case when s.stream_name ='MATHS & SCIENCE' then 1 else 0 end) AS SCIENCE, SUM(case when s.stream_name ='ALL' then 1 else 0 end) AS ALL_STREAMS, SUM(case when sbjct.subject_code = 'ACC' or sbjct.subject_code ='BUSMG' or sbjct.subject_code ='ECO'  then 1 else 0 end) AS COMMERCE_SUBJECTS FROM APP.STREAM s LEFT JOIN APP.SUBJECT sbjct ON s.stream_id = sbjct.stream_id", nativeQuery = true)
     Map<String, BigInteger> getMappedResults();
 
     @Query(value = "Select s FROM APP.Stream s fetch first row only", nativeQuery = true)
